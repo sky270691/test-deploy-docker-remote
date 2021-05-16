3a0fca9f570f84f178a3d02b4dff73d9fa137d69
Risky <langi.risky@gmail.com>
2021-05-16 22:51:20 +0800

    - reinitialize

---
+++ src/main/java/com/docker/testdeploydockerremote/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.docker.testdeploydockerremote.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public ResponseEntity<?> hello(){
+
+        return ResponseEntity.ok("KOMO");
+
+    }
+
+}
